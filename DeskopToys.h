a57cadb2ba643feb2b5980d04effe37b4d6b42c6
New91 <niu91@qq.com>
2019-12-15 14:22:23 +0800

    添加项目文件。

---
+++ DeskopToys.h
@@ -0,0 +1,32 @@
+﻿
+// DeskopToys.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CDeskopToysApp:
+// 有关此类的实现，请参阅 DeskopToys.cpp
+//
+
+class CDeskopToysApp : public CWinApp
+{
+public:
+	CDeskopToysApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDeskopToysApp theApp;
